5284e606d754cafb65820770d779838acea9abc8
Nick Gaugler <nickmgaugler@gmail.com>
2020-11-21 08:22:10 -0500

    v1.1

--- todo/urls.py
+++ todo/urls.py
@@ -15,9 +15,9 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
+#    path("", hello.views.index, name="index"),
+#    path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
     path('graphs/', include('graphs.urls'))
-    path('', include('tasks.urls'))
+    path('task/', include('tasks.urls'))
 ]
